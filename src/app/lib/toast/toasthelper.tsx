694c75bc29d8f29f9dcf7142b948619fd3698934
Your Name <you@example.com>
2025-06-20 17:12:10 +0400

    some_folder_place_change

---
+++ src/app/lib/toast/toasthelper.tsx
@@ -0,0 +1,10 @@
+import toast from 'react-hot-toast'
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+export const notifySuccess=(message:string, icon?: String)=>{
+    toast.success(`${message} ${!!icon ? icon : "" }`)
+}
+export const notifyError=(message:string)=>{
+    toast.custom(<AiOutlineCheckCircle size={24}/>)
+
+toast.error(message)
+}
